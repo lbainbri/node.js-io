fc8816bd4e31a9085553172ad7f10d15c0f62263
Luke Bainbridge <lbainbri@purdue.edu>
2020-03-31 18:21:34 -0400

    added formidable to the host

--- host.js
+++ host.js
@@ -1,6 +1,11 @@
-var http = require("http");
+var http = require("http"); //import http
+var formidable = require("formidable"); //import formidable
 
-http.createServer(function (req, res) {
-  console.log("Connected");
-  res.end();
+http.createServer(function (req, res) { //Create the server
+  console.log("Connected"); //log to the console when the client connects
+  var form = new formidable.IncomingForm(); //create a object for the form
+  form.parse(req, function (err, fields, files) { //parse the form
+    res.write('File uploaded');
+    res.end();
+  });
 }).listen(8080);
