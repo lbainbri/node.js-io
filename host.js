98bb80929db131c9e64faa4f27e7ee8c4020f9b1
Luke Bainbridge <lbainbri@purdue.edu>
2020-04-01 14:36:39 -0400

    added properly reading the file with utf8 encoding

--- host.js
+++ host.js
@@ -6,6 +6,10 @@ http.createServer(function (req, res) { //Create the server
   console.log("Connected"); //log to the console when the client connects
   if (req.url != "/fileupload") { //write out the client
     fs.readFile("client.html", function (err, data) {
+      if (err) {
+          console.error(err); //return if there is an error
+          return;
+      }
       res.write(data);
       console.log("Client started");
       res.end();
@@ -15,9 +19,17 @@ http.createServer(function (req, res) { //Create the server
     console.log("File uploaded");
     var form = new formidable.IncomingForm(); //create a object for the form
     form.parse(req, function (err, fields, files) { //parse the form
-      fs.readFile(files.uploadfile.name, function (err, data) { //read the file
+    //Read the file w/ utf8 (8-bit unicode tranformation format) encoding
+      fs.readFile(files.uploadfile.path, "utf8", function (err, data) {
+        if (err) {
+          console.error(err); //return if there is an error
+          return;
+        }
         console.log("File data:");
         console.log(data);
+        res.write("File data recieved\n");
+        res.write(data); //output the data of the file to the client
+        res.end();
       });
     });
   }
