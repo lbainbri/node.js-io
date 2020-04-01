ea40f3077b9cf0aab5856b1c2f7394f55e19bd26
Luke Bainbridge <lbainbri@purdue.edu>
2020-04-01 13:55:46 -0400

    have host output the file data to the console

--- host.js
+++ host.js
@@ -1,10 +1,14 @@
 var http = require("http"); //import http
 var formidable = require("formidable"); //import formidable
+var fs = require("fs"); //import filesystem
 
 http.createServer(function (req, res) { //Create the server
   console.log("Connected"); //log to the console when the client connects
   var form = new formidable.IncomingForm(); //create a object for the form
   form.parse(req, function (err, fields, files) { //parse the form
+    fs.readFile(files.uploadfile.name, function (err, data) { //read the file
+      console.log(data);
+    });
     res.write('File uploaded');
     res.end();
   });
