9228665f57f197c57a59ed9a52484b7634bb0a9f
Luke Bainbridge <lbainbri@purdue.edu>
2020-04-01 14:09:16 -0400

    made host start client when connected

--- host.js
+++ host.js
@@ -4,12 +4,21 @@ var fs = require("fs"); //import filesystem
 
 http.createServer(function (req, res) { //Create the server
   console.log("Connected"); //log to the console when the client connects
-  var form = new formidable.IncomingForm(); //create a object for the form
-  form.parse(req, function (err, fields, files) { //parse the form
-    fs.readFile(files.uploadfile.name, function (err, data) { //read the file
-      console.log(data);
+  if (req.url != "/fileupload") { //write out the client
+    fs.readFile("client.html", function (err, data) {
+      res.write(data);
+      console.log("Client started");
+      res.end();
     });
-    res.write('File uploaded');
-    res.end();
-  });
+  }
+  else {
+    console.log("File uploaded");
+    var form = new formidable.IncomingForm(); //create a object for the form
+    form.parse(req, function (err, fields, files) { //parse the form
+      fs.readFile(files.uploadfile.name, function (err, data) { //read the file
+        console.log("File data:");
+        console.log(data);
+      });
+    });
+  }
 }).listen(8080);
